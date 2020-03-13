4b50ca089b5ec42207d438cada3e21e6967e240d
erickempf <erickempf123@gmail.com>
2020-03-13 11:34:19 -0600

    Updated GUI

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import time
 from Maestro import Controller
 
-#Jalen Comment here and there
+
 ##-------------BEGIN GUI CLASS--------------------------------------
 
 MOTORS = 0
@@ -10,7 +10,6 @@ TURNING = 1
 BODY = 2
 HEAD_TURN = 3
 HEAD_TILT = 4
-#Eric commment
 
 class myGUI():
     def __init__(self,win):
@@ -53,6 +52,12 @@ class myGUI():
         self.trashy = 405 - self.size
         self.count=0
 
+        self.moveSleep = 2
+        self.turnSleep = 2
+        self.headTurnSleep = 2
+        self. headTiltSleep = 2
+        self.bodySleep = 2
+
     def controls(self):
         self.controls = tk.Canvas(win,bd = "0", bg="black", width=self.size+5, height="410")
         self.controls.create_rectangle(5,5,self.size,self.size,fill="red",outline="white")                     #MOVE
@@ -72,8 +77,8 @@ class myGUI():
         if event.x >= self.movex and event.x < self.movex+self.size and event.y>= self.movey and event.y<self.movey+self.size:
             self.console.create_rectangle(self.order[self.count][0],75,self.order[self.count][1],100,outline="black",fill="green")
             self.console.create_rectangle(self.order[self.count][0],300,self.order[self.count][1],325,outline="black",fill="red")
-            self.num.append(3)
-            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " Feet")
+            self.num.append(2)
+            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " S")
             self.buttons.append(0)
             print("MOVE")
             self.commandList.append(0)
@@ -82,11 +87,12 @@ class myGUI():
             self.console.create_rectangle(self.order[self.count][0],125,self.order[self.count][1],250,outline="Black",fill=self.order[self.count][2])
             self.count = self.count + 1
             self.console.pack(side=tk.LEFT)
+            self.incrementBoxes(event)
         if event.x >= self.turnx and event.x < self.turnx+self.size and event.y>= self.turny and event.y<self.turny+self.size:
-            self.num.append(45)
+            self.num.append(2)
             self.console.create_rectangle(self.order[self.count][0],75,self.order[self.count][1],100,outline="black",fill="green")
             self.console.create_rectangle(self.order[self.count][0],300,self.order[self.count][1],325,outline="black",fill="red")
-            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " Degrees")
+            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " S")
             self.buttons.append(1)
             print("TURN")
             self.commandList.append(1)
@@ -95,10 +101,10 @@ class myGUI():
             self.count = self.count + 1
             self.console.pack(side=tk.LEFT)
         if event.x >= self.headtiltx and event.x < self.headtiltx+self.size and event.y>= self.headtilty and event.y<self.headtilty+self.size:
-            self.num.append(20)
+            self.num.append(2)
             self.console.create_rectangle(self.order[self.count][0],75,self.order[self.count][1],100,outline="black",fill="green")
             self.console.create_rectangle(self.order[self.count][0],300,self.order[self.count][1],325,outline="black",fill="red")
-            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " Degrees")
+            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " S")
             self.buttons.append(2)
             print("TILT HEAD")
             self.commandList.append(2)
@@ -107,10 +113,10 @@ class myGUI():
             self.count = self.count + 1
             self.console.pack(side=tk.LEFT)
         if event.x >= self.headturnx and event.x < self.headturnx+self.size and event.y>= self.headturny and event.y<self.headturny+self.size:
-            self.num.append(20)
+            self.num.append(2)
             self.console.create_rectangle(self.order[self.count][0],75,self.order[self.count][1],100,outline="black",fill="green")
             self.console.create_rectangle(self.order[self.count][0],300,self.order[self.count][1],325,outline="black",fill="red")
-            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " Degrees")
+            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " S")
             self.buttons.append(3)
             print("TURN HEAD")
             self.commandList.append(3)
@@ -119,10 +125,10 @@ class myGUI():
             self.count = self.count + 1
             self.console.pack(side=tk.LEFT)
         if event.x >= self.bodyx and event.x < self.bodyx+self.size and event.y>= self.bodyy and event.y<self.bodyy+self.size:
-            self.num.append(20)
+            self.num.append(2)
             self.console.create_rectangle(self.order[self.count][0],75,self.order[self.count][1],100,outline="black",fill="green")
             self.console.create_rectangle(self.order[self.count][0],300,self.order[self.count][1],325,outline="black",fill="red")
-            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " Degrees")
+            self.console.create_text((self.order[self.count][0]+self.order[self.count][0]+self.size)/2 - 4,275, text = str(self.num[self.count]) + " S")
             self.buttons.append(4)
             print("BODY")
             self.commandList.append(4)
@@ -173,64 +179,68 @@ class myGUI():
         self.console.pack(side=tk.LEFT)
         return
 		
-	def incrementBoxes(self,event):
+    def incrementBoxes(self,event):
         print("Clicked")
         fill = "failed"
         if event.x >= self.order[0][0] and event.x <= self.order[0][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 0
+                self.num[0] += 2
+                print(self.num[0])
                 fill = 10
             if event.y >=300 and event.y <= 325:
-                #Decrement num 0
+                self.num[0] -=2
+                print(self.num[0])
                 fill = 1
         if event.x >= self.order[1][0] and event.x <= self.order[1][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 1
+                self.num[1] += 2
+                print(self.num[1])
                 fill = 1
             if event.y >=300 and event.y <= 325:
-                #Decrement num 1
+                self.num[1] -=2
+                print(self.num[1])
                 fill = -1
         if event.x >= self.order[2][0] and event.x <= self.order[2][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 2
+                self.num[2] +=2
                 fill = 2
             if event.y >=300 and event.y <= 325:
-                #Decrement num 2
+                self.num[2] -=2
                 fill = -2
         if event.x >= self.order[3][0] and event.x <= self.order[3][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 3
+                self.num[3] +=2
                 fill = 3
             if event.y >=300 and event.y <= 325:
-                #Decrement num 3
+                self.num[3] -=2
                 fill = -3
         if event.x >= self.order[4][0] and event.x <= self.order[4][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 4
+                self.num[4] +=2
                 fill = 4
             if event.y >=300 and event.y <= 325:
-                #Decrement num 4
+                self.num[4] -=2
                 print("decrement 4")
         if event.x >= self.order[5][0] and event.x <= self.order[5][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 1
+                self.num[5] +=2
                 fill = 5
             if event.y >=300 and event.y <= 325:
-                #Decrement num 1
+                self.num[5] -=2
                 fill = -5
         if event.x >= self.order[6][0] and event.x <= self.order[6][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 2
+                self.num[6] +=2
                 fill = 6
             if event.y >=300 and event.y <= 325:
-                #Decrement num 2
+                self.num[6] -=2
                 fill = -6
         if event.x >= self.order[7][0] and event.x <= self.order[7][1]:
             if event.y >=75 and event.y <= 100:
-                #Increment num 3
+                self.num[7] += 2
                 fill = 7
             if event.y >=300 and event.y <= 325:
-                #Decrement num 3
+                self.num[7] -=2
                 fill = -7
         print(fill)
         pass
@@ -240,6 +250,7 @@ class myGUI():
       return
 
     def execute(self, commandList):
+        print("Execute")
         for item in commandList:
             self.body = 6000
             self.headTurn = 6000
@@ -251,35 +262,35 @@ class myGUI():
                 self.motors = 5000
                 self.tango.setTarget(MOTORS, self.motors)
                 print("stuff")
-                time.sleep(1)
+                time.sleep(self.num[item])
                 self.motors = 6000
                 self.tango.setTarget(MOTORS, self.motors)
             elif(item == 1):
                 self.turn = 5000
                 self.tango.setTarget(TURNING, self.turn)
                 print("stuff")
-                time.sleep(1)
+                time.sleep(self.num[item])
                 self.turn = 6000
                 self.tango.setTarget(TURNING, self.turn)
             elif(item == 2):
                 self.headTilt = 5000
                 self.tango.setTarget(HEAD_TILT, self.headTilt)
                 print("stuff")
-                time.sleep(1)
+                time.sleep(self.num[item])
                 self.headTilt = 6000
                 self.tango.setTarget(HEAD_TILT, self.headTilt)
             elif(item == 3):
                 self.headTurn = 5000
                 self.tango.setTarget(HEAD_TURN, self.headTurn)
                 print("stuff")
-                time.sleep(1)
+                time.sleep(self.num[item])
                 self.headTurn = 6000
                 self.tango.setTarget(HEAD_TURN, self.headTurn)
             elif(item == 4):
                 self.body = 5000
                 self.tango.setTarget(BODY, self.body)
                 print("stuff")
-                time.sleep(1)
+                time.sleep(self.num[item])
                 self.body = 6000
                 self.tango.setTarget(BODY, self.body)
             time.sleep(1)
