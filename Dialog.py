0cacc9de84089b3fc93675e31b0772ebe4abb104
erickempf <erickempf123@gmail.com>
2020-04-06 17:35:10 -0600

    Added dialog functionality

--- Dialog.py
+++ Dialog.py
@@ -8,6 +8,7 @@ class entry():
         self.children = []
         self.parent = None
 
+
 class dialog():
     def __init__(self,text):
         
@@ -18,6 +19,7 @@ class dialog():
         self.definitionsList = [] #Definitions header location
         self.dialogList = [] #Saves all possible diaglog options
         self.recentEntry = entry(0,"error","error")
+        self.validinput = []
 
     def getLevel(self):
         return self.level
@@ -36,28 +38,29 @@ class dialog():
         print("Proposition")
         pass
 
-    def options(self, command, human, robot): # Handles all other dialog            <------- Working on this
+    def options(self, command, human, robot): # Handles all other dialog
         entry1 = entry(command, human, robot)
         if entry1.command == 0:
             self.dialogList.append(entry1)
-            print("if1")
+            self.validinput.append(human)
+            #print("if1")
         elif command == self.recentEntry.command+1:
             self.recentEntry.children.append(entry1)
             entry1.parent = self.recentEntry
-            print("if2")
+            #print("if2")
         elif command == self.recentEntry.command:
             parent = self.recentEntry.parent
             parent.children.append(entry1)
             entry1.parent = parent
-            print("if3")
+            #print("if3")
         else:
             while(self.recentEntry.command >= command):
                 self.recentEntry = self.recentEntry.parent
             self.recentEntry.children.append(entry1)
             entry1.parent = self.recentEntry
-            print("if4")
-        recentEntry = entry1
-        print(entry1.command)
+            #print("if4")
+        self.recentEntry = entry1
+        #print(entry1.command)
 
     def textIn(self):           #Reads in the file text
         file = open(self.text,"r")
@@ -101,19 +104,42 @@ class dialog():
 dialogEng = dialog("dialog.txt")
 
 dialogEng.textIn()
-#for entry in dialogEng.dialogList:          #Printing out everything to test
-#    print(entry.command+"|"+entry.human+"|"+entry.robot)
-    
 run = True
 while(run):
     human = input()
     human = human.lower()
     if human == "exit":
         run = False
-    for entry in dialogEng.dialogList:
-        if entry.human == human:
-            print(entry.robot)
-print("program finished")
+    elif human in dialogEng.validinput:
+        for entry in dialogEng.dialogList:
+            if entry.human == human:
+                print(entry.robot)
+                if entry.children != []:
+                    currentEntry = entry
+                    dialogEng.level += 1
+                    while dialogEng.level != 0:
+                        validinputs = []
+                        for child in currentEntry.children:
+                            validinputs.append(child.human)
+                        human = input()
+                        if human in validinputs:
+                            for child in currentEntry.children:
+                                if child.human == human:
+                                    print(child.robot)
+                                    currentEntry = child
+                                    if currentEntry.children != []:
+                                        dialogEng.level +=1
+                                    else:
+                                        dialogEng.level = 0
+                        elif human == "exit":
+                            dialogEng.level = 0
+                            run = False
+                        else:
+                            print("Invalid input")                        
+    else:
+        print("Invalid input")
+        
+print("Goodbye. Program finished")
     
 
 
